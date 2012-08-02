c9a87803e0d7016c7ac7b0c9cfb59da475cbd0ce
LWarstone@gmail.com <LWarstone@gmail.com@76fb6fa6-f94b-9468-eb41-b63ae2c71f15>
2012-08-02 17:10:34 +0000

    [UOExt.GUI]   Issue 13: Logo changed.

--- trunk/UOExt.gui/GUIThread.pas
+++ trunk/UOExt.gui/GUIThread.pas
@@ -32,15 +32,23 @@ type
     FUpdateEvent: THandle;
     FUpdateEventDone: THandle;
     FFreeEvent: THandle;
+
     FWindow: HWND;
     FFont: HFONT;
-    FUpdateRect: TRect;
-    FBufferBitmap: HBITMAP;
     FBufferDC: HDC;
+    FRawDC: HDC;
+    FRawImage: HBITMAP;
+    FSelectedImage: HBITMAP;
+    FSize: TSize;
+    FWindowPosition: TPoint;
+    FZeroPoint: TPoint;
+    FUpdateRect: TRect;
+    FShowedLines: Byte;
 
     FInsertData: TUpdatingLine;
     FLastInsertedHandle: Cardinal;
     procedure InvokeUpdateLog;
+    procedure InvokeUpdateWindow;
   protected
     function Execute: Integer; override;
   public
@@ -56,103 +64,143 @@ implementation
 
 // Local procedures
 
-function GUIWindowProcA(hWnd: HWND; Msg: UINT; wParam: WPARAM; lParam: LPARAM): LRESULT; stdcall;
+procedure gblur(src:Pointer; Width: Cardinal; Height: Cardinal; Radius: Cardinal);
 var
-  Thread: TGUIThread;
-  bProcessed: Boolean;
-  DC: HDC;
-  i: Byte;
-  Text: PAnsiString;
-  rct: TRect;
-  cY: Integer;
-  CurrentData: PDataLine;
-  iMaxPos, iCurrPos: Integer;
-  GWLA: Integer;
-  PaintInfo: tagPAINTSTRUCT;
-  oBrush, cBrush: HBRUSH;
+  r: Real;
+  N: Cardinal;
+  window: Array of Real;
+  s2, Sum: Real;
+  i, j, k: Cardinal;
+  l: Integer;
+  pTmp: Pointer;
+  tmpRow: Pointer;
 Begin
-  Result := 0;
-  GWLA := GetWindowLongA(hWnd, GWL_USERDATA);
-  Thread := TGUIThread(GWLA);
-  bProcessed := False;
-  If Thread <> nil Then case Msg of
-    WM_QUIT: Begin
-      Thread.FNeedExit := True;
-    End;
-    WM_SIZE: Begin
-      If Thread.FBufferBitmap <> 0 then DeleteObject(Thread.FBufferBitmap);
-      if Thread.FBufferDC <> 0 then DeleteObject(Thread.FBufferDC);
-      DC := GetDC(hWnd);
-      Thread.FBufferDC := CreateCompatibleDC(DC);
-      Thread.FBufferBitmap := CreateCompatibleBitmap(DC, lParam AND $FFFF, lParam SHR 16);
-      ReleaseDC(hWnd, DC);
-      SelectObject(Thread.FBufferDC, Thread.FBufferBitmap);
-      SelectObject(Thread.FBufferDC, GetClassLongA(hWnd, GCL_HBRBACKGROUND));
-      SelectObject(Thread.FBufferDC, Thread.FFont);
-    End;
-    WM_ERASEBKGND: Begin
-      Result := 1;
-      bProcessed := True;
-    End;
-    WM_PAINT: Begin
-      SelectObject(Thread.FBufferDC, GetClassLongA(hWnd, GCL_HBRBACKGROUND));
-      GetClientRect(hWnd, rct);
-      Rectangle(Thread.FBufferDC, 0, 0, rct.Right - rct.Left, rct.Bottom - rct.Top);
-      SetBkMode(Thread.FBufferDC, TRANSPARENT);
-      DC := Thread.FBufferDC;
+  N := Radius;
+  s2 := 2*N*N / 9;
+  SetLength(window, N*2);
 
-      cY := 160;
-      iMaxPos := High(Thread.FDataLines);
-      if iMaxPos > 3 then iMaxPos := 3;
-      if iMaxPos >= 0 then For i := 0 to iMaxPos do Begin
-        iCurrPos := High(Thread.FDataLines);
-        if iCurrPos <= 3 then iCurrPos := 0 else iCurrPos := iCurrPos - 3;
+  window[N] := 1;
+  for i := 1 to N - 1 do Begin
+    window[N + i] := Exp(-i*i/s2);
+    window[N - i] := window[N + i];
+  End;
 
-        CurrentData := @Thread.FDataLines[iCurrPos + i];
-        Text := @CurrentData^.Text;
-        rct.Left := 30 + CurrentData^.TotalOffset * 5;
-        rct.Top := cY;
-        rct.Bottom := 0;
-        rct.Right := 0;
-        DrawTextExA(DC, @Text^[1], Length(Text^), rct, DT_CALCRECT XOR DT_LEFT XOR DT_TOP XOR DT_SINGLELINE XOR DT_NOPREFIX, nil);
-        DrawTextExA(DC, @Text^[1], Length(Text^), rct, DT_LEFT XOR DT_TOP XOR DT_SINGLELINE XOR DT_NOPREFIX, nil);
-        cY := rct.Bottom + 1;
-        if CurrentData^.ProgressMax > 0 then Begin
-          if rct.Right + 5 < 290 then Begin
-            rct.Left := rct.Right + 5;
-            rct.Right := 290;
-            rct.Top := rct.Top + 1;
-            rct.Bottom := rct.Bottom - 1;
-            Rectangle(DC, rct.Left, rct.Top, rct.Right, rct.Bottom);
-
-            If CurrentData^.Progress > CurrentData^.ProgressMax then CurrentData^.Progress := CurrentData^.ProgressMax;
-            If(rct.Right - rct.Left > 4) Then Begin
-              rct.Right := rct.Right - 2;
-              rct.Left := rct.Left + 2;
-            End;
-            rct.Right := ((rct.Right - rct.Left) * CurrentData^.Progress) DIV (CurrentData^.ProgressMax - CurrentData^.ProgressMin) + rct.Left;
-            If(rct.Bottom - rct.Top > 4) Then Begin
-              rct.Top := rct.Top + 2;
-              rct.Bottom := rct.Bottom - 2;
-            End;
-            cBrush := CreateSolidBrush(0);
-            oBrush := SelectObject(DC, cBrush);
-            Rectangle(DC, rct.Left, rct.Top, rct.Right, rct.Bottom);
-            SelectObject(DC, oBrush);
-            DeleteObject(cBrush);
-          End;
+  tmpRow := GetMemory(4*width);
+  ZeroMemory(tmpRow, 4*width);
+  for j := 0 to Height - 1 do Begin
+    for i := 0 to Width - 1 do Begin
+      Sum := 0;
+      r := 0;
+      for k := 0 to 2*N - 1 do Begin
+        l := i + k - N;
+        If(l>=0)and (Cardinal(l) < Width) then Begin
+          pTmp := Pointer(Cardinal(src) + (j*Width + Cardinal(l))*4);
+          r := r + PByte(Cardinal(pTmp) + 2)^ * window[k];
+          sum := sum + window[k];
         End;
       End;
-
-      GetClientRect(hWnd, rct);
-      DC := BeginPaint(hWnd, PaintInfo);
-      BitBlt(DC, 0, 0, rct.Right - rct.Left, rct.Bottom - rct.Top, Thread.FBufferDC, 0, 0, SRCCOPY);
-      EndPaint(DC, PaintInfo);
-      bProcessed := True;
+      pTmp := Pointer(Cardinal(tmpRow) + i*4);
+      PByte(Cardinal(pTmp) + 2)^ := Round(r / sum);
     End;
-  end;
-  if not bProcessed then Result := DefWindowProcA(hWnd, Msg, wParam, lParam);
+    CopyMemory(Pointer(Cardinal(src) + (j*width)*4), tmpRow, width*4);
+  End;
+  FreeMemory(tmpRow);
+
+  tmpRow := GetMemory(4*Height);
+  ZeroMemory(tmpRow, 4*Height);
+  for i := 0 to width - 1 do Begin
+    for j := 0 to Height - 1 do Begin
+      Sum := 0;
+      r := 0;
+      for k := 0 to 2*N do Begin
+        l := j + k - N;
+        If(l>=0)and (Cardinal(l) < Height) then Begin
+          pTmp := Pointer(Cardinal(src) + (Cardinal(l)*Width + i)*4);
+          r := r + PByte(Cardinal(pTmp) + 2)^ * window[k];
+          sum := sum + window[k];
+        End;
+      End;
+      pTmp := Pointer(Cardinal(tmpRow) + (j)*4);
+      PByte(Cardinal(pTmp) + 2)^ := Round(r / sum);
+    End;
+    for j := 0 to Height - 1 do Begin
+      PCardinal(Cardinal(src) + (j*width + i)*4)^ := PCardinal(Cardinal(tmpRow) + j*4)^;
+    End;
+  End;
+  FreeMemory(tmpRow);
 End;
+
+procedure RedToAlpha(src:Pointer; Width: Cardinal; Height: Cardinal);
+var
+  i: Cardinal;
+  pTmp: Pointer;
+Begin
+  pTmp := src;
+  for i := 0 to Width * Height - 1 do Begin
+    if PByte(Cardinal(pTmp) + 2)^ = 0 then Begin
+      PByte(Cardinal(pTmp) + 3)^ := $00;
+    End Else Begin
+      PByte(Cardinal(pTmp) + 3)^ := PByte(Cardinal(pTmp) + 2)^;
+      PByte(Cardinal(pTmp) + 2)^ := $00;
+    End;
+    pTmp := Pointer(Cardinal(pTmp) + 4);
+  End;
+End;
+
+procedure RenderImage(var DC:HDC; var Img:HBITMAP);
+var
+  screenDC: HDC;
+  bFont, mFont, oFont: HFONT;
+  BMIH: tagBITMAPINFO;
+  pImg: Pointer;
+Begin
+  if DC <> INVALID_HANDLE_VALUE then Begin
+    DeleteObject(Img);
+    DeleteDC(DC);
+  End;
+
+  screenDC := GetDC(0);
+  DC := CreateCompatibleDC(screenDC);
+  ReleaseDC(0, screenDC);
+  BMIH.bmiHeader.biSize := SizeOf(BMIH.bmiHeader);
+  BMIH.bmiHeader.biWidth := 320;
+  BMIH.bmiHeader.biHeight := 240;
+  BMIH.bmiHeader.biPlanes := 1;
+  BMIH.bmiHeader.biBitCount := 32;
+  BMIH.bmiHeader.biCompression := BI_RGB;
+
+  Img := CreateDIBSection(DC, BMIH, 0, pImg, 0, 0);
+  SelectObject(DC, Img);
+
+  bFont := CreateFontA(-MulDiv(64, GetDeviceCaps(DC, LOGPIXELSY), 72), 0, 0, 0, FW_BOLD, 0, 0, 0, ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH, 'Arial');
+  mFont := CreateFontA(-MulDiv(20, GetDeviceCaps(DC, LOGPIXELSY), 72), 0, 0, 0, FW_BOLD, 0, 0, 0, ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH, 'Arial');
+
+  oFont := SelectObject(DC, bFont);
+  SetTextColor(DC, $FF);
+  SetBkMode(DC, TRANSPARENT);
+
+  TextOutA(DC, 31, 5, 'UOExt', 5);
+  SelectObject(DC, mFont);
+
+  TextOutA(DC, 45, 95, 'We do what EA can''t', 19);
+  SelectObject(DC, bFont);
+
+  gblur(pImg, 320, 240, 15);
+
+  TextOutA(DC, 26, 0, 'UOExt', 5);
+  SelectObject(DC, mFont);
+
+  TextOutA(DC, 40, 90, 'We do what EA can''t', 19);
+  SelectObject(DC, oFont);
+
+  DeleteObject(bFont);
+  DeleteObject(mFont);
+
+  gblur(pImg, 320, 240, 3);
+  RedToAlpha(pImg, 320, 240);
+
+End;
+
 
 // TGUIThread
 
@@ -162,30 +210,40 @@ var
   ClassAtom: Word;
 
   Msg: TMsg;
-  hBackgroundImage: HBITMAP;
-  DC: HDC;
   WaitEvents: Array[0..1] of THandle;
   tmsg: tagMSG;
 Begin
   Result := 1;
   PeekMessage(tmsg, 0, WM_USER, WM_USER, PM_NOREMOVE);
 
-  hBackgroundImage := LoadBitmapA(HInstance, 'MAINIMAGE');
+//  hBackgroundImage := LoadBitmapA(HInstance, 'MAINIMAGE');
 
+  RenderImage(FRawDC, FRawImage);
+  FSize.cx := 320;
+  FSize.cy := 240;
+  FWindowPosition.X := (GetSystemMetrics(SM_CXSCREEN) - FSize.cx) DIV 2;
+  FWindowPosition.Y := (GetSystemMetrics(SM_CYSCREEN) - FSize.cy) DIV 2;
+  FZeroPoint.X := 0;
+  FZeroPoint.Y := 0;
+
+  FUpdateRect.Left := 25;
+  FUpdateRect.Right := 295;
+  FUpdateRect.Top := 160;
+  FUpdateRect.Bottom := 220;
+  FShowedLines := 3;
 
   WndClass.cbSize := SizeOf(WndClass);
   WndClass.style := 0;
-  WndClass.lpfnWndProc := @GUIWindowProcA;
+  WndClass.lpfnWndProc := @DefWindowProcA;
   WndClass.cbClsExtra := 0;
-  WndClass.cbWndExtra := 4;
+  WndClass.cbWndExtra := 0;
   WndClass.hInstance := 0;
   WndClass.hIcon := 0;
   WndClass.hCursor := 0;
-  WndClass.hbrBackground := CreatePatternBrush(hBackgroundImage);
+  WndClass.hbrBackground := 0;
   WndClass.lpszMenuName := nil;
   WndClass.lpszClassName := 'UOExt.GUI';
   WndClass.hIconSm := LoadCursor(0, IDC_ARROW);
-  DeleteObject(hBackgroundImage);
 
   UnregisterClassA('UOExt.GUI', 0);
   ClassAtom := RegisterClassExA(WndClass);
@@ -193,27 +251,13 @@ Begin
 
   SetLength(FDataLines, 0);
 
-
-  FWindow := CreateWindowExA(WS_EX_TOPMOST XOR WS_EX_TOOLWINDOW, 'UOExt.GUI', nil, WS_POPUP XOR WS_VISIBLE XOR WS_SYSMENU XOR WS_VISIBLE, (GetSystemMetrics(SM_CXSCREEN) - 320) DIV 2, (GetSystemMetrics(SM_CYSCREEN) - 240) DIV 2, 320, 240, 0, 0, 0, nil);
-  FUpdateRect.Left := 30;
-  FUpdateRect.Right := 290;
-  FUpdateRect.Top := 160;
-  FUpdateRect.Bottom := 210;
-
-  SetWindowLongA(FWindow, GWL_USERDATA, Integer(Self));
+  FWindow := CreateWindowExA(WS_EX_TOPMOST XOR WS_EX_TOOLWINDOW XOR WS_EX_LAYERED, 'UOExt.GUI', nil, WS_POPUP XOR WS_VISIBLE XOR WS_SYSMENU XOR WS_VISIBLE, FWindowPosition.X, FWindowPosition.Y, FSize.cx, FSize.cy, 0, 0, 0, nil);
   If FWindow = 0 then Exit;
 
   FFont := CreateFont(-MulDiv(7, GetDeviceCaps(GetWindowDC(FWindow), LOGPIXELSY), 72), 0, 0, 0, FW_NORMAL, 0, 0, 0, ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH, 'Arial');
   if FFont = 0 then Exit;
   SendMessageA(FWindow, WM_SETFONT, FFont, 0);
-
-  DC := GetDC(FWindow);
-  FBufferDC := CreateCompatibleDC(DC);
-  FBufferBitmap := CreateCompatibleBitmap(DC, 320, 240);
-  ReleaseDC(FWindow, DC);
-  SelectObject(FBufferDC, FBufferBitmap);
-  SelectObject(FBufferDC, WndClass.hbrBackground);
-  SelectObject(FBufferDC, FFont);
+  InvokeUpdateWindow;
   ShowWindow(FWindow, SW_SHOW);
 
   FUpdateEvent := CreateEventA(nil, False, False, 'UpdateEvent');
@@ -247,6 +291,111 @@ Begin
   CloseHandle(FUpdateEventFree);
 End;
 
+procedure TGUIThread.InvokeUpdateWindow;
+var
+  ScreenDC, TmpDC: HDC;
+  BF: TBlendFunction;
+  iMaxPos, iCurrPos, i, cY, iX, iY: Integer;
+  CurrentData: PDataLine;
+  Text: PAnsiString;
+  rct: TRect;
+  oBrush, cBrush: HBRUSH;
+  BMIH: tagBITMAPINFO;
+  pImg: Pointer;
+  BMP: HBITMAP;
+begin
+  ScreenDC := GetDC(0);
+  if FBufferDC <> 0 then DeleteDC(FBufferDC);
+  FBufferDC := CreateCompatibleDC(ScreenDC);
+
+  If FSelectedImage <> 0 Then DeleteObject(FSelectedImage);
+  FSelectedImage := CopyImage(FRawImage, IMAGE_BITMAP, 0, 0, 0);
+  SelectObject(FBufferDC, FSelectedImage);
+
+
+  TmpDC := CreateCompatibleDC(0);
+  SelectObject(TmpDC, FFont);
+  BMIH.bmiHeader.biSize := SizeOf(BMIH.bmiHeader);
+  BMIH.bmiHeader.biWidth := FUpdateRect.Right - FUpdateRect.Left;
+  BMIH.bmiHeader.biHeight := FUpdateRect.Bottom - FUpdateRect.Top;
+  BMIH.bmiHeader.biPlanes := 1;
+  BMIH.bmiHeader.biBitCount := 32;
+  BMIH.bmiHeader.biCompression := BI_RGB;
+  BMP := CreateDIBSection(TmpDC, BMIH, 0, pImg, 0, 0);
+  SelectObject(TmpDC, BMP);
+  SetBkColor(TmpDC, $FFFFFF);
+  SetBkMode(TmpDC, TRANSPARENT);
+  SetTextColor(TmpDC, 0);
+
+  cBrush := CreateSolidBrush($FFFFFF);
+  oBrush := SelectObject(TmpDC, cBrush);
+  FloodFill(TmpDC, 0, 0, $FF);
+  SelectObject(TmpDC, oBrush);
+  DeleteObject(cBrush);
+
+
+  iMaxPos := High(FDataLines);
+  iCurrPos := iMaxPos;
+  cY := 0;
+  if iCurrPos <= 3 then iCurrPos := 0 else iCurrPos := iCurrPos - 3;
+  if iMaxPos > 3 then iMaxPos := 3;
+  if iMaxPos >= 0 then For i := 0 to iMaxPos do Begin
+    CurrentData := @FDataLines[iCurrPos + i];
+    Text := @CurrentData^.Text;
+    rct.Left := CurrentData^.TotalOffset * 5;
+    rct.Top := cY;
+    rct.Bottom := 0;
+    rct.Right := 0;
+    DrawTextExA(TmpDC, @Text^[1], Length(Text^), rct, DT_CALCRECT XOR DT_LEFT XOR DT_TOP XOR DT_SINGLELINE XOR DT_NOPREFIX, nil);
+    DrawTextExA(TmpDC, @Text^[1], Length(Text^), rct, DT_LEFT XOR DT_TOP XOR DT_SINGLELINE XOR DT_NOPREFIX, nil);
+    cY := rct.Bottom + 1;
+    if CurrentData^.ProgressMax > 0 then Begin
+      if rct.Right + 5 < (FUpdateRect.Right - FUpdateRect.Left) then Begin
+        rct.Left := rct.Right + 5;
+        rct.Right := (FUpdateRect.Right - FUpdateRect.Left);
+        rct.Top := rct.Top + 1;
+        rct.Bottom := rct.Bottom - 1;
+        Rectangle(TmpDC, rct.Left, rct.Top, rct.Right, rct.Bottom);
+
+        If CurrentData^.Progress > CurrentData^.ProgressMax then CurrentData^.Progress := CurrentData^.ProgressMax;
+        If(rct.Right - rct.Left > 4) Then Begin
+          rct.Right := rct.Right - 2;
+          rct.Left := rct.Left + 2;
+        End;
+        rct.Right := ((rct.Right - rct.Left) * CurrentData^.Progress) DIV (CurrentData^.ProgressMax - CurrentData^.ProgressMin) + rct.Left;
+        If(rct.Bottom - rct.Top > 4) Then Begin
+          rct.Top := rct.Top + 2;
+          rct.Bottom := rct.Bottom - 2;
+        End;
+        cBrush := CreateSolidBrush(0);
+        oBrush := SelectObject(TmpDC, cBrush);
+        Rectangle(TmpDC, rct.Left, rct.Top, rct.Right, rct.Bottom);
+        SelectObject(TmpDC, oBrush);
+        DeleteObject(cBrush);
+      End;
+    End;
+  End;
+  if iMaxPos > 0 Then for iX := 0 to BMIH.bmiHeader.biWidth - 1 do for iY := 0 to BMIH.bmiHeader.biHeight - 1 do Begin
+    PByte(Cardinal(pImg) + 3)^ := 255 - PByte(Cardinal(pImg) + 2)^;
+    ZeroMemory(pImg, 3);
+    pImg := Pointer(Cardinal(pImg) + 4);
+  End;
+
+  BF.BlendOp := AC_SRC_OVER;
+  BF.BlendFlags := 0;
+  BF.SourceConstantAlpha := $FF;
+  BF.AlphaFormat := AC_SRC_ALPHA;
+  AlphaBlend(FBufferDC, FUpdateRect.Left, FUpdateRect.Top, FUpdateRect.Right - FUpdateRect.Left, FUpdateRect.Bottom - FUpdateRect.Top, TmpDC, 0, 0, FUpdateRect.Right - FUpdateRect.Left, FUpdateRect.Bottom - FUpdateRect.Top, BF);
+  DeleteDC(TmpDC);
+  DeleteObject(BMP);
+
+  BF.SourceConstantAlpha := $FF;
+  BF.AlphaFormat := AC_SRC_ALPHA;
+
+  UpdateLayeredWindow(FWindow, ScreenDC, @FWindowPosition, @FSize, FBufferDC, @FZeroPoint, 0, @BF, ULW_ALPHA);
+  ReleaseDC(0, ScreenDC);
+end;
+
 
 procedure TGUIThread.InvokeUpdateLog;
 var
@@ -276,8 +425,7 @@ begin
       FDataLines[iPos].TotalOffset := FDataLines[FInsertData.ParentLine].TotalOffset + 1;
   End;
 
-  InvalidateRect(FWindow, @FUpdateRect, True);
-  UpdateWindow(FWindow);
+  InvokeUpdateWindow;
 
   FLastInsertedHandle := iPos;
   SetEvent(FUpdateEventDone);
