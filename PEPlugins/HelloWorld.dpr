1ee88dcd7db6745605bd9a13508c0b505faeb6db
LWarstone <LWarstone@gmail.com>
2012-10-29 21:13:16 +0000

    [Core]   Switch to WinSock 2.2. This boost performance.   SendPacket now fully thread safe (example in HelloWorld.plg)

--- PEPlugins/HelloWorld.dpr
+++ PEPlugins/HelloWorld.dpr
@@ -3,7 +3,8 @@ library HelloWorld;
 uses
   PluginsShared in '..\UOExt\PluginsShared.pas',
   PluginAPI in 'Common\PluginAPI.pas',
-  plgHelloWorld in 'plgHelloWorld.pas';
+  plgHelloWorld in 'plgHelloWorld.pas',
+  AbstractThread in '..\UOExt\AbstractThread.pas';
 
 {$E plg}
 exports PluginAPI.DllInit, PluginAPI.DllInitDone;
